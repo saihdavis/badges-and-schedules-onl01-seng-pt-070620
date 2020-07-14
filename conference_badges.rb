8e305b61fe6cd0909e78f483e96cdb000bcad50e
Saida Davis <saihdavis@gmail.com>
2020-07-14 00:21:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,13 +3,13 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-binding.pry
+
 def batch_badge_creator(attendees)
   attendees.collect do |attendee|
     badge_maker(attendee)
   end
 end
-
+binding.pry
 def assign_rooms(attendees)
   attendees.map.with_index do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
